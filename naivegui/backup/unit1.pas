a670d7b1d276556772446aba202e7ad7d123abac
AKotov-dev <akotov830@gmail.com>
2026-03-18 17:47:20 +0300

    v0.1 + sing-box-1.13.3

---
+++ naivegui/backup/unit1.pas
@@ -0,0 +1,246 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons,
+  ExtCtrls, Process;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    ZoneBox: TComboBox;
+    DomainEdit: TEdit;
+    SPortEdit: TEdit;
+    HPortEdit: TEdit;
+    Label11: TLabel;
+    LogMemo: TMemo;
+    Shape1: TShape;
+    StaticText1: TStaticText;
+    URIEdit: TEdit;
+    Label10: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    UserEdit: TEdit;
+    PasswordEdit: TEdit;
+    Label1: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    StartBtn: TSpeedButton;
+    StopBtn: TSpeedButton;
+    SpeedButton3: TSpeedButton;
+    procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure SpeedButton3Click(Sender: TObject);
+    procedure StartBtnClick(Sender: TObject);
+    procedure StopBtnClick(Sender: TObject);
+    procedure CreateClientConfig;
+    procedure CreateServerConfig;
+
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+uses start_trd, service_state_trd, JsonArrayHelper;
+
+  {$R *.lfm}
+
+  { TMainForm }
+
+
+//Создаём конфиг клиента
+procedure TMainForm.CreateClientConfig;
+var
+  S: TStringList;
+begin
+  try
+    S := TStringList.Create;
+    S.Add('{');
+    S.Add('  "log": {');
+    S.Add('    "level": "info"');
+    S.Add('  },');
+    S.Add('');
+    S.Add('  "dns": {');
+    S.Add('    "servers": [');
+    S.Add('      { "tag": "remote", "type": "udp", "server": "1.1.1.1" },');
+    S.Add('      { "tag": "local",  "type": "udp", "server": "8.8.4.4" }');
+    S.Add('    ],');
+    S.Add('    "rules": [');
+    S.Add('      { "domain_suffix": ["' + ZoneBox.Text + '"], "server": "local" }');
+    S.Add('    ]');
+    S.Add('  },');
+    S.Add('');
+    S.Add('  "inbounds": [');
+    S.Add('    {');
+    S.Add('      "type": "socks",');
+    S.Add('      "listen": "127.0.0.1",');
+    S.Add('      "listen_port": ' + SPortEdit.Text);
+    S.Add('    },');
+    S.Add('    {');
+    S.Add('      "type": "http",');
+    S.Add('      "listen": "127.0.0.1",');
+    S.Add('      "listen_port": ' + HPortEdit.Text);
+    S.Add('    }');
+    S.Add('  ],');
+    S.Add('');
+    S.Add('  "outbounds": [');
+    S.Add('    {');
+    S.Add('      "type": "naive",');
+    S.Add('      "tag": "proxy",');
+    S.Add('      "server": "' + DomainEdit.Text + '",');
+    S.Add('      "server_port": 443,');
+    S.Add('      "username": "' + UserEdit.Text + '",');
+    S.Add('      "password": "' + PasswordEdit.Text + '",');
+    S.Add('      "tls": {');
+    S.Add('        "enabled": true,');
+    S.Add('        "server_name": "' + DomainEdit.Text + '"');
+    S.Add('      }');
+    S.Add('    },');
+    S.Add('    {');
+    S.Add('      "type": "direct",');
+    S.Add('      "tag": "direct"');
+    S.Add('    }');
+    S.Add('  ],');
+    S.Add('');
+    S.Add('  "route": {');
+    S.Add('    "rules": [');
+    S.Add('      {');
+    S.Add('        "domain_suffix": ["' + ZoneBox.Text + '"],');
+    S.Add('        "outbound": "direct"');
+    S.Add('      }');
+    S.Add('    ],');
+    S.Add('    "final": "proxy",');
+    S.Add('   "default_domain_resolver": "remote"');
+    S.Add('  }');
+    S.Add('}');
+
+    S.SaveToFile(GetUserDir + '.config/naivegui/client.json');
+
+  finally
+    S.Free
+  end;
+end;
+
+//Создаём конфиг Сервера
+procedure TMainForm.CreateServerConfig;
+var
+  S: TStringList;
+begin
+  try
+    S := TStringList.Create;
+
+    S.Add('{');
+    S.Add('    # Важно: forward_proxy обрабатывается перед file_server');
+    S.Add('    order forward_proxy first');
+    S.Add('}');
+    S.Add('');
+    S.Add(DomainEdit.Text + ' {');
+    S.Add('');
+    S.Add('    # 1. Forward proxy с авторизацией');
+    S.Add('    forward_proxy {');
+    S.Add('        basic_auth ' + UserEdit.Text + ' "' + PasswordEdit.Text + '"');
+    S.Add('        hide_ip');
+    S.Add('        hide_via');
+    S.Add('        probe_resistance');
+    S.Add('    }');
+    S.Add('');
+    S.Add('    # 2. Маскировка обычным сайтом');
+    S.Add('    file_server {');
+    S.Add('        root /var/www/html');
+    S.Add('    }');
+    S.Add('}');
+
+    //Для /etc/caddy/Caddyfile на сервере
+    S.SaveToFile(GetUserDir + '.config/naivegui/Caddyfile');
+  finally
+    S.Free;
+  end;
+
+end;
+
+procedure TMainForm.FormCreate(Sender: TObject);
+var
+  FShowLogTRD, FServiceStateTRD: TThread;
+begin
+  MainForm.Caption := Application.Title;
+
+  //Запуск потока проверки состояния сервиса (active/inactive)
+  FServiceStateTRD := ServiceState.Create(False);
+  FServiceStateTRD.Priority := tpNormal;
+
+  //Запуск поток непрерывного чтения лога
+  FShowLogTRD := ShowLogTRD.Create(False);
+  FShowLogTRD.Priority := tpNormal;
+end;
+
+procedure TMainForm.FormShow(Sender: TObject);
+var
+  client_conf: string;
+begin
+  client_conf := GetUserDir + '.config/naivegui/client.json';
+  if not FileExists(client_conf) then Exit;
+
+  //Читаем параметры клиента и сервера
+  DomainEdit.Text := JsonReadString(client_conf, 'outbounds[0].server');
+  //  PortEdit.Text := JsonReadString(client_conf, 'outbounds[0].server_port');
+  UserEdit.Text := JsonReadString(client_conf, 'outbounds[0].username');
+  PasswordEdit.Text := JsonReadString(client_conf, 'outbounds[0].password');
+
+  SPortEdit.Text := JsonReadString(client_conf, 'inbounds[0].listen_port');
+  HPortEdit.Text := JsonReadString(client_conf, 'inbounds[1].listen_port');
+
+  ZoneBox.Text := JsonReadString(client_conf, 'dns.rules[0].domain_suffix[0]');
+end;
+
+//Создаём конфиги Клиента и Сервера
+procedure TMainForm.SpeedButton3Click(Sender: TObject);
+begin
+  //Клиент
+  CreateClientConfig;
+  //Сервер
+  CreateServerConfig;
+end;
+
+//Start + Enable
+procedure TMainForm.StartBtnClick(Sender: TObject);
+var
+  S: string;
+begin
+  //Не запускать, если поля пустые
+  if (DomainEdit.Text = '') or (UserEdit.Text = '') or (PasswordEdit.Text = '') or
+    (SPortEdit.Text = '') or (HPortEdit.Text = '') or (ZoneBox.Text = '') then Exit;
+
+  //Не запускать ДО создания конфига Клиента и Сервера
+  if not FileExists(GetUserDir + '.config/naivegui/client.json') then Exit;
+
+  //Пересоздаём конфиг клиента
+  CreateClientConfig;
+
+  RunCommand('systemctl', ['--user', 'restart', 'naivegui.service'], S, [poWaitOnExit]);
+  RunCommand('systemctl', ['--user', 'enable', 'naivegui.service'], S, [poWaitOnExit]);
+end;
+
+//Stop + Disable
+procedure TMainForm.StopBtnClick(Sender: TObject);
+var
+  S: string;
+begin
+  RunCommand('systemctl', ['--user', 'stop', 'naivegui.service'], S, [poWaitOnExit]);
+  RunCommand('systemctl', ['--user', 'disable', 'naivegui.service'], S, [poWaitOnExit]);
+end;
+
+end.
---
+++ naivegui/naivegui.lpr
@@ -0,0 +1,29 @@
+program naivegui;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}
+  cthreads,
+  {$ENDIF}
+  {$IFDEF HASAMIGA}
+  athreads,
+  {$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, Unit1, start_trd, service_state_trd, JsonArrayHelper
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Title:='NaiveGUI v0.1';
+  Application.Scaled:=True;
+  {$PUSH}{$WARN 5044 OFF}
+  Application.MainFormOnTaskbar:=True;
+  {$POP}
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
+
