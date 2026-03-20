ffb5f71c4406d209e2c917950528f383cf91041e
AKotov-dev <akotov830@gmail.com>
2026-03-20 12:39:56 +0300

    v0.1 + sing-box-1.13.3

--- naivegui/backup/unit1.pas
+++ naivegui/backup/unit1.pas
@@ -46,6 +46,7 @@ type
     procedure StopBtnClick(Sender: TObject);
     procedure CreateClientConfig;
     procedure CreateServerConfig;
+    procedure CreateSWProxy;
 
   private
 
@@ -64,6 +65,87 @@ uses start_trd, service_state_trd, JsonArrayHelper;
 
   { TMainForm }
 
+
+//Create ~/config/naivegui/swproxy.sh
+procedure TMainForm.CreateSWProxy;
+var
+  S: ansistring;
+  A: TStringList;
+begin
+  try
+    A := TStringList.Create;
+    A.Add('#!/bin/bash');
+    A.Add('');
+    A.Add('if [[ "$1" == "set" ]]; then');
+    A.Add('  echo "set proxy..."');
+    A.Add('');
+    A.Add('  # GNOME / GTK-based');
+    A.Add('  if [[ "$XDG_CURRENT_DESKTOP" =~ GNOME|Budgie|Cinnamon|MATE|XFCE|LXDE ]]; then');
+    A.Add('    gsettings set org.gnome.system.proxy mode manual');
+    A.Add('    gsettings set org.gnome.system.proxy.http  host "127.0.0.1"');
+    A.Add('    gsettings set org.gnome.system.proxy.http  port ' + HPortEdit.Text);
+    A.Add('    gsettings set org.gnome.system.proxy.https host "127.0.0.1"');
+    A.Add('    gsettings set org.gnome.system.proxy.https port ' + HPortEdit.Text);
+    A.Add('    gsettings set org.gnome.system.proxy.ftp   host "127.0.0.1"');
+    A.Add('    gsettings set org.gnome.system.proxy.ftp   port ' + HPortEdit.Text);
+    A.Add('    gsettings set org.gnome.system.proxy.socks host "127.0.0.1"');
+    A.Add('    gsettings set org.gnome.system.proxy.socks port ' +
+      SPortEdit.Text);
+    A.Add('    gsettings set org.gnome.system.proxy ignore-hosts "[' +
+      '''' + 'localhost' + '''' + ', ' + '''' + '127.0.0.1' + '''' +
+      ', ' + '''' + '::1' + '''' + ']"');
+    A.Add('  fi');
+    A.Add('');
+    A.Add('  # KDE Plasma');
+    A.Add('  if [[ "$XDG_CURRENT_DESKTOP" == KDE ]]; then');
+    A.Add('    if command -v kwriteconfig5 >/dev/null; then');
+    A.Add('      v=5');
+    A.Add('    elif command -v kwriteconfig6 >/dev/null; then');
+    A.Add('      v=6');
+    A.Add('    else');
+    A.Add('      echo "No kwriteconfig found"');
+    A.Add('      exit 1');
+    A.Add('  fi');
+    A.Add('');
+    A.Add('    kwriteconfig$v --file kioslaverc --group "Proxy Settings" --key ProxyType 1');
+    A.Add('    kwriteconfig$v --file kioslaverc --group "Proxy Settings" --key httpProxy  "http://127.0.0.1:' + HPortEdit.Text + '"');
+    A.Add('    kwriteconfig$v --file kioslaverc --group "Proxy Settings" --key httpsProxy "http://127.0.0.1:' + HPortEdit.Text + '"');
+    A.Add('    kwriteconfig$v --file kioslaverc --group "Proxy Settings" --key ftpProxy   "http://127.0.0.1:' + HPortEdit.Text + '"');
+    A.Add('    kwriteconfig$v --file kioslaverc --group "Proxy Settings" --key socksProxy "socks5h://127.0.0.1:' + SPortEdit.Text + '"');
+    A.Add('    kwriteconfig$v --file kioslaverc --group "Proxy Settings" --key NoProxy    "['
+      + '''' + 'localhost' + '''' + ', ' + '''' + '127.0.0.1' +
+      '''' + ', ' + '''' + '::1' + '''' + ']"');
+    A.Add('  fi');
+    A.Add('else');
+    A.Add('  echo "unset proxy..."');
+    A.Add('');
+    A.Add('  # GNOME / GTK-based');
+    A.Add('  if [[ "$XDG_CURRENT_DESKTOP" =~ GNOME|Budgie|Cinnamon|MATE|XFCE|LXDE ]]; then');
+    A.Add('    gsettings set org.gnome.system.proxy mode none');
+    A.Add('  fi');
+    A.Add('');
+    A.Add('  # KDE Plasma');
+    A.Add('  if [[ "$XDG_CURRENT_DESKTOP" == KDE ]]; then');
+    A.Add('    if command -v kwriteconfig5 >/dev/null; then');
+    A.Add('      v=5');
+    A.Add('    elif command -v kwriteconfig6 >/dev/null; then');
+    A.Add('      v=6');
+    A.Add('    else');
+    A.Add('      echo "No kwriteconfig found"');
+    A.Add('      exit 1');
+    A.Add('    fi');
+    A.Add('');
+    A.Add('    kwriteconfig$v --file kioslaverc --group "Proxy Settings" --key ProxyType 0');
+    A.Add('  fi');
+    A.Add('fi');
+    A.Add('');
+
+    A.SaveToFile(GetUserDir + '.config/naivegui/swproxy.sh');
+    RunCommand('/bin/bash', ['-c', 'chmod +x ~/.config/naivegui/swproxy.sh'], S);
+  finally
+    A.Free;
+  end;
+end;
 
 //Создаём конфиг клиента
 procedure TMainForm.CreateClientConfig;
@@ -207,7 +289,6 @@ begin
     if Trim(S) = '' then QUICBox.Checked := True
     else
       QUICBox.Checked := False;
-    ShowMessage(S);
   end;
 
   client_conf := GetUserDir + '.config/naivegui/client.json';
@@ -249,6 +330,10 @@ begin
   //Пересоздаём конфиг клиента
   CreateClientConfig;
 
+  //Пересоздаём пускач прокси
+  CreateSWProxy;
+  RunCommand('/bin/bash', ['-c', '~/.config/naivegui/swproxy.sh set'], S);
+
   RunCommand('systemctl', ['--user', 'restart', 'naivegui.service'], S, [poWaitOnExit]);
   RunCommand('systemctl', ['--user', 'enable', 'naivegui.service'], S, [poWaitOnExit]);
 end;
@@ -258,6 +343,10 @@ procedure TMainForm.StopBtnClick(Sender: TObject);
 var
   S: string;
 begin
+    //Пересоздаём пускач прокси
+  CreateSWProxy;
+  RunCommand('/bin/bash', ['-c', '~/.config/naivegui/swproxy.sh', 'unset'], S);
+
   RunCommand('systemctl', ['--user', 'stop', 'naivegui.service'], S, [poWaitOnExit]);
   RunCommand('systemctl', ['--user', 'disable', 'naivegui.service'], S, [poWaitOnExit]);
 end;
